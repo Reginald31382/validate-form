da6afb79e3010f23d95af113b514664c19872ac3
Reginald J Dixon <vassarjonez@yahoo.com>
2024-03-14 02:06:17 -0400

    completed form validation with Yup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import MyForm from "./components/MyForm";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Example of a validated form 2024</h1>
+      <MyForm />
+    </div>
+  );
+}
+
+export default App;
