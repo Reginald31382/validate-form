cb18fb7e88cfaaf71852dc02cd42e274b218490d
Reginald J Dixon <vassarjonez@yahoo.com>
2024-05-10 14:11:39 -0400

    update

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,24 +2,48 @@ import React from "react";
 
 const Modal = () => {
   return (
-    <div>
-      <h2>Quality Information</h2>
-      <ul>
-        <li>Basic</li>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta,
-          sint.
-        </p>
-        <li>Standard</li>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste,
-          tempore.
-        </p>
-        <li>Premium</li>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum,
-          obcaecati?
-        </p>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+
+        border: "2px solid black",
+        borderRadius: "5px",
+        padding: "10px",
+      }}
+    >
+      <h2 style={{ textAlign: "center" }}>Quality Information</h2>
+      <span style={{ textAlign: "center" }}>
+        <strong>Card Size:</strong> 2"x3.5"
+      </span>
+      <ul
+        style={{ listStyleType: "circle", width: "100%", placeSelf: "center" }}
+      >
+        <li>
+          <strong>Basic</strong>
+          <br /> <span>Slightly Thick Gloss</span> <br />
+          <span>100-lb 2-Sided Gloss Cover</span>
+          <br />
+          <sup style={{ marginLeft: "3px" }}>Feels slick and glossy</sup>
+        </li>
+        <br />
+        <li>
+          <strong>Standard</strong>
+          <br /> <span>Thick Gloss</span>
+          <br />
+          <span>114-lb/14-pt 2-Sided Digital Gloss Cover</span>
+          <br />
+          <sup style={{ marginLeft: "3px" }}>Feels slick and glossy</sup>
+        </li>
+        <br />
+        <li>
+          <strong>Premium</strong> <br />
+          <span>Extra Thick Gloss</span>
+          <br />
+          <span>122-lb/16-pt Gloss Cover</span>
+          <br />
+          <sup style={{ marginLeft: "3px" }}>Feels slick and glossy</sup>
+        </li>
       </ul>
     </div>
   );
